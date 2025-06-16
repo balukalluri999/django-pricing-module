35d6f62c4bb37e303412d8e823e2aff1edce2d99
balukalluri999 <balarajaiah.k20@iiits.in>
2025-06-16 07:46:48 +0530

    Register PricingConfig in Django admin with search and filters

--- pricing_project/pricing/admin.py
+++ pricing_project/pricing/admin.py
@@ -9,11 +9,5 @@ class PricingConfigAdmin(admin.ModelAdmin):
     list_filter = ('is_active', 'days_active')
     search_fields = ('name',)
 
-@admin.register(PricingConfig)
-class PricingConfigAdmin(admin.ModelAdmin):
-    list_display = ('name', 'base_distance', 'base_price', 'is_active', 'created_at')
-    list_filter = ('is_active', 'days_active')
-    search_fields = ('name',)
-
 admin.site.register(TimeMultiplier)
 admin.site.register(WaitingCharge)
